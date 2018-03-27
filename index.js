b8d1317198cd2aa5cf6ba40c4ca30aab26eb262d
Ehab Khaireldin <ehab.khaireldin@onemeter.com>
2018-03-27 17:10:31 +0200

    v2.1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=> {
-	const version = { current: 'v2' };
+	const version = { current: 'v2.1' };
 	res.status(200).json(version);
 });
 

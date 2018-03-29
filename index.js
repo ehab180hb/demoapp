7ed370f5b02b020a6589f660e500e6cf3cde06f6
Ehab Khaireldin <ehab.khaireldin@onemeter.com>
2018-03-29 10:12:53 +0200

    vSup4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=> {
-	const version = { current: 'vSup3' };
+	const version = { current: 'vSup4' };
 	res.status(200).json(version);
 });
 
